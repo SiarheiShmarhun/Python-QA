7b821d325ae57f0481006c1bc04eff15da7bbe96
Siarhei Shmarhun <shmarhunsiarhei@gmail.com>
2026-03-08 00:12:34 +0300

    Fix: resolve all linting issues for Pylint and Flake8

--- homework5.py
+++ homework5.py
@@ -1,6 +1,9 @@
+"""Homework 5: String and List methods solutions."""
+# pylint: disable=invalid-name, line-too-long
+
 # Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'.
 url = 'www.my_site.com#about'
-print(url.replace('#','/'))
+print(url.replace('#', '/'))
 
 
 # Напишите программу, которая добавляет ‘ing’ к словам.
@@ -19,12 +22,14 @@ text = '   hello world   '
 print(text.strip())
 
 
-# Имена собственные всегда начинаются с заглавной буквы, за которой следуют строчные буквы. Исправьте данное имя собственное так, чтобы оно соответствовало этому утверждению.
+# Имена собственные всегда начинаются с заглавной буквы, за которой следуют строчные буквы.
+# Исправьте данное имя собственное так, чтобы оно соответствовало этому утверждению.
 name = 'siArHeI'
 print(name.capitalize())
 
 
-# Перевести строку в список "Robin Singh" => ["Robin", "Singh"], "I love arrays they are my favorite" => ["I", "love", "arrays", "they", "are", "my", "favorite"].
+# Перевести строку в список "Robin Singh" => ["Robin", "Singh"]
+# "I love arrays they are my favorite" => ["I", "love", "arrays", "they", "are", "my", "favorite"].
 string_1 = "Robin Singh"
 string_2 = "I love arrays they are my favorite"
 arr1 = string_1.split()
@@ -33,20 +38,23 @@ arr2 = string_2.split()
 print(arr2)
 
 
-# Дан список: [Robin Singh], и 2 строки: "Welcome" и "airport". Напечатайте текст: “Hello, Robin Singh! Welcome to airport”
+# Дан список: [Robin Singh], и 2 строки: "Welcome" и "airport".
+# Напечатайте текст: “Hello, Robin Singh! Welcome to airport”
 names = ["Robin", "Singh"]
 action = "Welcome"
 place = "airport"
 print(f"Hello, {names[0]} {names[1]}! {action} to {place}")
 
 
-# Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] сделайте из него строку => "I love arrays they are my favorite"
+# Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
+# сделайте из него строку => "I love arrays they are my favorite"
 words_list = ["I", "love", "arrays", "they", "are", "my", "favorite"]
 print(' '.join(words_list))
 
 
-# Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение, удалите элемент из списка под индексом 6
+# Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение
+# удалите элемент из списка под индексом 6
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers.insert(2, 777)
-del numbers [6]
+del numbers[6]
 print(numbers)
